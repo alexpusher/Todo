bc7f21fb3a782a01b0a1804b23f4806aaaaf8409
Alex <alexpusher1@yandex.ru>
2018-02-17 16:23:14 +0300

    add delete and edit task

--- app/Home.vue
+++ app/Home.vue
@@ -10,10 +10,11 @@
           <input type="text" placeholder="Input task" v-model="inputTask" @keyup.enter="addTask()"/>
         </div>
         <div class="tasks" v-if="tasks">
-          <div class="task" v-for="task in tasks">
-            <label>{{task.name}}</label>
-            <button>Edit</button>
-            <button>Delete</button>
+          <div class="task" v-for="(task, id) in tasks" :data-task-id="id">
+            <input v-if="task.isEdit" v-model="task.text" @keyup.enter="closeEdit($event)" @keyup.esc="closeEdit($event)" type="text" placeholder="Input task"/>
+            <label v-else v-on:dblclick="editTask($event)">{{task.text}}</label>
+            <button v-on:click="closeTask($event)">Close task</button>
+            <button v-on:click="deleteTask($event)">Delete</button>
           </div>
         </div>
       </div>
@@ -28,6 +29,7 @@
 *   0 - deleted task
 */
 <script>
+  const $ = require('jquery');
     export default {
         data(){
             return {
@@ -41,12 +43,33 @@
         methods:{
           // Add new task
           addTask(){
+            if(this.inputTask.length <= 0) return;
             let task = {
-              name: this.inputTask,
+              text: this.inputTask,
               status: 1,
+              isEdit: false
             };
             this.tasks.push(task);
             this.inputTask = '';
+          },
+          deleteTask(ev){
+            let item = $(ev.currentTarget).parent();
+            this.$delete(this.tasks,item.attr('data-task-id'));
+          },
+          editTask(ev){
+            let item = $(ev.currentTarget).parent();
+            this.tasks[item.attr('data-task-id')].isEdit = true;
+            //this.tasks[item.attr('data-task-id')].isEdit = true;
+
+
+          },
+          closeEdit(ev){
+            let item = $(ev.currentTarget).parent();
+            this.tasks[item.attr('data-task-id')].isEdit = false;
+          },
+          closeTask(ev){
+            let item = $(ev.currentTarget).parent();
+            //this.tasks[item.attr('data-task-id')].isEdit = false;
           }
         }
     }
--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Home from './Home.vue';
 
 
+
 export default new Vue({
     el:'#app',
     render: h => h(Home)
